331ee7b60926923cdeff269dc6abdd448e19ba0d
Chan Tsz Ching <id506507@gmail.com>
2019-06-13 17:59:40 +0800

    add  new thingsssssssss

--- calculator/calculator.vue
+++ calculator/calculator.vue
@@ -5,25 +5,35 @@
             <input type="string" v-model="result">
         </div>
         <div class="display">
+<!--row1-->
             <button class="special" @click="clear">C</button>
             <button class="special" @click="del">&larr;</button>
             <button class="special" @click="percent">%</button>
+            <button class="special" @click="power">x<sup>2</sup></button>
             <button class="operator" @click="add('/')">/</button>
+<!--row12-->
             <button class="item" @click="add(7)">7</button>
             <button class="item" @click="add(8)">8</button>
             <button class="item" @click="add(9)">9</button>
+            <button class="item" @click="sin">sin</button>
             <button class="operator" @click="add('*')">*</button>
+<!--row3-->
             <button class="item" @click="add(4)">4</button>
             <button class="item" @click="add(5)">5</button>
             <button class="item" @click="add(6)">6</button>
+            <button class="item" @click="cos">cos</button>
             <button class="operator" @click="add('-')">-</button>
+<!--row4-->
             <button class="item" @click="add(1)">1</button>
             <button class="item" @click="add(2)">2</button>
             <button class="item" @click="add(3)">3</button>
+            <button class="item" @click="tan">tan</button>
             <button class="operator" @click="add('+')">+</button>
+<!--row5-->
             <button class="item zero" @click="add(0)">0</button>
             <button class="item" @click="add('.')">.</button>
             <button class="operator" @click="calc">=</button>
+
         </div>
     </div>
     <div class="log_area">
@@ -73,6 +83,20 @@ export default {
             this.logs.push(this.result+" = "+eval(this.result));
             this.result=eval(this.result);
             this.enter=true;
+        },
+        power:function(){
+            //this.result=Math.pow(this.result,2);
+            let power=this.result*this.result;
+            this.add(power);
+        },
+        sin:function(){
+            this.result=Math.sin(this.result);
+        },
+        cos:function(){
+            this.result=Math.cos(this.result);
+        },
+        tan:function(){
+            this.result=Math.tan(this.result);
         }
     }
 }
@@ -85,7 +109,6 @@ export default {
 }
 .log_font{
     font-size: 200%;
-    float: left;
 }
 .log_area>p{
     font-size: 120%;
@@ -93,7 +116,7 @@ export default {
 
 .calculator{
     background-color: #2f2f31;
-    width: 33.8%;
+    
     font-family: Arial, Helvetica, sans-serif;
     float: left;    
 }
@@ -114,8 +137,7 @@ export default {
 .display {
   
     display: grid;
-    grid-template-columns: 100px 100px 100px 100px;
-    grid-auto-flow: row dense;
+    grid-template-columns: 100px 100px 100px 100px 100px;
     grid-gap: 2px;
     background-color: #2f2f31;
     padding: 10px;
@@ -142,7 +164,7 @@ export default {
 }
 .zero{
     grid-column-start: 1;
-    grid-column-end: 3;
+    grid-column-end: 4;
 }
 button:active{
     background-color: #2f2f31;
